572a53337c23db04c7c187cd3507fa570b137964
Newton Musyimi <28211658+Newton-Musyimi@users.noreply.github.com>
2022-08-02 18:29:04 +0200

    Initial Project Files
    Initial project files such as the home page that I, Newton, used to test the links in order to adjust for his server back at home

---
+++ employees.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Core Group</title>
+    <meta http-equiv="Cache-control" content="no-store">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon.png">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body id="page-top">
+    <header>
+    </header>
+    <footer class="bg-white sticky-footer" style="padding-bottom: 32px;">
+        <div class="container my-auto">
+            <div class="text-center my-auto copyright"><span>Copyright © Newton Musyimi 2022</span></div>
+        </div>
+    </footer>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bs-init.js"></script>
+    <script src="/assets/js/admin_page.js"></script>
+    <script src="/assets/js/app.js"></script>
+    <script src="/assets/js/employees_control.js"></script>
+</body>
+
+</html>
---
+++ scripts/login.php
@@ -0,0 +1,62 @@
+<?php
+require_once('newpamo_config.php');
+if (isset($_SESSION['id'])) {
+    if($_SESSION['role']=='client' || $_SESSION['role']=='receptionist'){
+        header("location: /helpdesk.php");
+    }else{
+        header("location: /workorders.php");
+    }
+}
+
+function standardize($string)
+{
+    $string = str_replace(' ', '', $string);
+    $string = stripslashes($string);
+    return strtolower($string);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE) OR die("Could not connect to MySQL: " . mysqli_connect_error() . "<br>Contact IT for assistance!");
+    $username = standardize($_POST['username']);
+    $password = $_POST['password'];
+    $query = "SELECT `username`, `user_type`, `password` FROM `users` WHERE `username` = $username;";
+    $result = mysqli_query($conn, $query) or die("<p class='access_form'>Log in <span style='color:red;'>failed!</span> Username entered is incorrect.</p> " . $conn->error);
+    
+    $row = mysqli_fetch_array($result);
+    $hashed_pass = $row['password'];
+    if(password_verify($password, $hashed_pass)){
+        if($row['user_type'] == 'client'){
+            $_SESSION['role'] = $row['user_type'];
+        }else{
+            $result = mysqli_query($conn, "SELECT `role_id` FROM `user_role` WHERE `user_id` = ".$row['user_id'].";");
+
+            if ($row = mysqli_fetch_array($result)){
+                $result = mysqli_query($conn, "SELECT `role_id` FROM `user_role` WHERE `user_id` = ".$row['user_id'].";");
+                if ($row = mysqli_fetch_array($result)){
+                    $result = mysqli_query($conn, "SELECT `role_name` FROM `roles` WHERE `role_id` = ".$row['role_id'].";");
+                    if ($row = mysqli_fetch_array($result)){
+                        $_SESSION['role'] = $row['role_name'];
+                    }else{
+                        $_SESSION['role'] = 'employee';
+                    }
+                }else{
+                    $_SESSION['role'] = 'employee';
+                }
+            }else{
+                $_SESSION['role'] = 'employee';
+            }
+        }
+        $_SESSION['logged_in'] = $row['user_id'];
+        $_SESSION['username'] = $row['username'];
+        
+        $row = mysqli_fetch_array($result);
+        $_SESSION['rank'] = $row['rank'];
+        echo 'true';
+        
+    }else{
+        echo '<p>You have entered the wrong password. Try again!</p>';
+        exit;
+    }
+    
+    mysqli_close($conn);
+}
