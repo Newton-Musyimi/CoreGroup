8f6da0b43578935c046def3392de1fda16fd9c9e
Musyimi <newtonthemaker@gmail.com>
2022-08-14 21:37:48 +0200

    Update header.php
    Updated the header file to change dynamically based on the user's role

--- header.php
+++ header.php
@@ -1,33 +1,84 @@
 <?php
-/*
-function getHeader($role){
-    echo "<h1>$role header here</h1>";
-}
+$host = "http://".$_SERVER['HTTP_HOST'];
 function getAdminHeader(){
-    echo '<h1>admin header here</h1>';
+    global $host;
+    echo "<div class=\"navbar\">
+    <nav class=\"top-nav\">
+        <ul>
+            <li><a href=\"$host/SysDev/CoreGroup/admin.php\"  class=\"nav-item\">Admin</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/test.php\"  class=\"nav-item\">Test</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/employees.php\"  class=\"nav-item\">Employees</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/workorders.php\"  class=\"nav-item\">Workorders</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/scripts/logout.php\"  class=\"nav-item\">Log Out</a> </li>
+        </ul>
+    </nav>
+</div>";
 }
 
 function getReceptionistHeader(){
+    global $host;
+    echo "<div class=\"navbar\"
+<nav class=\"top-nav\">
+    <ul>
+        <li><a href=\"$host/SysDev/CoreGroup/workorders.php\"  class=\"nav-item\">Admin</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/helpdesk.php\"  class=\"nav-item\">Test</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/employees.php\"  class=\"nav-item\">Employees</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/workorders.php\"  class=\"nav-item\">Workorders</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/scripts/logout.php\"  class=\"nav-item\">Log Out</a> </li>
+    </ul>
+</nav>
+</div>";
 
 }
- */
-session_start();
+
+function getClientHeader(){
+    global $host;
+    echo "<div class=\"navbar\"
+<nav class=\"top-nav\">
+    <ul>
+        <li><a href=\"$host/SysDev/CoreGroup/workorders.php\"  class=\"nav-item\">Workorders</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/devices.php\"  class=\"nav-item\">Devices</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/scripts/logout.php\"  class=\"nav-item\">Log Out</a> </li>
+    </ul>
+</nav>
+</div>";
+}
+function getEmployeeHeader(){
+    global $host;
+    echo "<div class=\"navbar\"
+    <nav class=\"top-nav\">
+        <ul>
+            <li><a href=\"$host/SysDev/CoreGroup/workorders.php\"  class=\"nav-item\">Workorders</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/devices.php\"  class=\"nav-item\">Devices</a> </li>
+            <li><a href=\"$host/SysDev/CoreGroup/scripts/logout.php\"  class=\"nav-item\">Log Out</a> </li>
+        </ul>
+    </nav>
+    </div>";
+}
 function getHeader(){
+    global $host;
     if(isset($_SESSION['logged_in'])){
-        echo "<nav class=\"top-nav\">
-    <ul>
-        <li><a href=\"/SysDev/CoreGroup/admin.php\">Admin</a> </li>
-        <li><a href=\"/SysDev/CoreGroup/test.php\">Test</a> </li>
-        <li><a href=\"/SysDev/CoreGroup/scripts/logout.php\">Log Out</a> </li>
-    </ul>
-</nav>";
+        $role = $_SESSION['role'];
+        echo "You are a(n) $role<br>";
+        if($role == 'ADMINISTRATOR'){
+            getAdminHeader();
+        }elseif($role == 'RECEPTIONIST'){
+            getReceptionistHeader();
+        }elseif($role == 'CLIENT'){
+            getClientHeader();
+        }else{
+            echo '<h1>employee header here</h1>';
+        }
     }else{
-        echo "<nav class=\"top-nav\">
+        echo "<div class=\"navbar\"
+<nav class=\"top-nav\">
     <ul>
-        <li><a href=\"/SysDev/CoreGroup/admin.php\">Admin</a> </li>
-        <li><a href=\"/SysDev/CoreGroup/test.php\">Test</a> </li>
-        <li><a href=\"/SysDev/CoreGroup/login.php\">Log In</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/admin.php\">Admin</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/test.php\">Test</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/login.php\">Log In</a> </li>
+        <li><a href=\"$host/SysDev/CoreGroup/signup.php\">Sign Up</a> </li>
     </ul>
-</nav>";
+</nav>
+</div>";
     }
 }
