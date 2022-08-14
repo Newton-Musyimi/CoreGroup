99e11dfd092e6698eb2c955566d558b74ad58a27
Musyimi <newtonthemaker@gmail.com>
2022-08-14 21:43:46 +0200

    Admin pages
    Updated the 2 main admin pages to have some tables and forms for adding employees

--- admin.php
+++ admin.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 require_once($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/security/admin/config.php');
-require_once ($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/header.php');
-
+require_once($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/header.php');
 ?>
 <!DOCTYPE html>
 <html lang="en-gb">
@@ -12,28 +11,86 @@ require_once ($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/header.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Core Group</title>
     <meta http-equiv="Cache-control" content="no-store">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/css/style.css';?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php global $host; echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
 </head>
 
 <body id="page-top">
-    <header>
+    <header value="admin">
         <?php
         getHeader();
         ?>
     </header>
-    
+
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <?php
+        $conn = get_db();
+        $sql = "SELECT * FROM employees";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['title'] . "</td>";
+                echo "<td>" . $row['first_name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['mobile'] . "</td>";
+                echo "<td>" . $row['address'] . "</td>";
+                echo "</tr>";
+
+            }
+        }
+        ?>
+        </tbody>
+        <tfoot>
+        <tr>
+            <th>Title</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+        </tr>
+        </tfoot>
+    </table>
+
+    <div class='tab'>
+        <button class='tablinks' onclick='openTab(event, "Reports")'>Reports</button>
+        <button class='tablinks' onclick='openTab(event, "Settings")'>Settings</button>
+    </div>
+    <div id='Reports' class='tabcontent'>
+        <h3>Reports</h3>
+        <p>Reports</p>
+    </div>
+    <div id='Settings' class='tabcontent'>
+        <h3>Settings</h3>
+        <p>Settings</p>
+    </div>
+
     <footer style="padding-bottom: 32px;">
         <div class="container my-auto">
             <div class="text-center my-auto copyright"><span>Copyright © Core Group 2022</span></div>
         </div>
     </footer>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/jquery.min.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/logout.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/jquery.min.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/logout.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
+                </div>
+            </div>
+            </div>
+    </div>
 </body>
 
 </html>
--- employees.php
+++ employees.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+require_once($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/security/admin/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/header.php');
+
+//require_once('/SysDev/CoreGroup/security/admin/config.php');
+?>
 <!DOCTYPE html>
 <html lang="en-gb">
 
@@ -6,24 +13,116 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Core Group</title>
     <meta http-equiv="Cache-control" content="no-store">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/css/style.css';?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php global $host; echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
 </head>
 
 <body id="page-top">
     <header>
+        <?php
+        getHeader();
+        ?>
     </header>
+    <div class='tab'>
+        <button class="tablinks" onclick="openTab(event, 'Employees')" id="defaultOpen">Employees</button>
+        <button class="tablinks" onclick="openTab(event, 'AddEmployee')">Add Employee</button>
+    </div>
+    <div id='Employees' class='tabcontent'>
+        <h3>Employees</h3>
+        <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                <?php
+                $conn = get_db();
+                $sql = "SELECT * FROM employees";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['title'] . "</td>";
+                        echo "<td>" . $row['first_name'] . "</td>";
+                        echo "<td>" . $row['email'] . "</td>";
+                        echo "<td>" . $row['mobile'] . "</td>";
+                        echo "<td>" . $row['address'] . "</td>";
+                        echo "</tr>";
+
+                    }
+                }
+                ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                    <th>Title</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+    <div id="AddEmployee" class="tabcontent">
+        <h3>Add Employee</h3>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type="hidden" name="title" value="TECHNICIAN">
+            <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name">
+            </div>
+            <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="mobile">Mobile</label>
+                <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile">
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input type="text" class="form-control" id="address" name="address" placeholder="Address">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <?php
+            if(isset($_REQUEST['title'])){
+                $title = $_REQUEST['title'];
+                $first_name = $_REQUEST['first_name'];
+                $last_name = $_REQUEST['last_name'];
+                $email = $_REQUEST['email'];
+                $mobile = $_REQUEST['mobile'];
+                $address = $_REQUEST['address'];
+                $sql = "INSERT INTO employees (title, first_name, last_name, email, mobile, address) VALUES ('$title', '$first_name', '$last_name', '$email', '$mobile', '$address');";
+                if ($conn->query($sql) === TRUE) {
+                    echo "<p style='color: green'>$first_name $last_name has been added to the database</p>";
+                } else {
+                    echo "<p style='color: darkred'><strong style='color: red'>Error:</strong> Could not add $first_name $last_name into the databse</p> <br><p style='color: red'><strong>Message: </strong>" .$conn->error."";
+                }
+                $conn ->close();
+            }
+            ?>
+        </form>
+    </div>
     <footer class="bg-white sticky-footer" style="padding-bottom: 32px;">
         <div class="container my-auto">
             <div class="text-center my-auto copyright"><span>Copyright © Newton Musyimi 2022</span></div>
         </div>
     </footer>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/jquery.min.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/logout.js';?>"></script>
-    <script src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
+    <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
 </body>
 
 </html>
