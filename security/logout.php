22a6dca78ce14784ec30b0c49b5e2671ded4e367
Musyimi <newtonthemaker@gmail.com>
2022-08-15 02:05:49 +0200

    Moved files
    Moved delete_account and logout scripts from the scripts folder to the security folder

--- security/logout.php
+++ security/logout.php
@@ -1,11 +1,10 @@
 <?php
     session_start();
-    $root = "http://{$_SERVER['HTTP_HOST']}/SysDev/CoreGroup/";
+    $root = "http://{$_SERVER['HTTP_HOST']}/SysDev/CoreGroup";
     if(session_unset() && session_destroy()){
         header("location: $root/index.php");
-        //header("location: /SysDev/CoreGroup/index.php");
+        //header("location: /SysDev/CoreGroup/");
     }else{
         header("location: $root/scripts/logout.php");
         //header("location: /SysDev/CoreGroup/scripts/logout.php");
     }
-?>
