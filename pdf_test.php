647f677b7acb3cdd1697aaa841f8b4e797ced066
Newton Musyimi <28211658+Newton-Musyimi@users.noreply.github.com>
2022-08-31 14:09:38 +0200

    Update pdf_test.php

--- pdf_test.php
+++ pdf_test.php
@@ -1,69 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+<?php
 
-    <style>
-        section {
-            background-color: #80b8e9;
-        }
-    </style>
-</head>
-<body>
-<section id="section">
-    <h1>Heyyy</h1>
-</section>
+header("Content-Type: application/octet-stream");
 
-<button onclick="printPdf()">print</button>
+$file = $_GET["file"] . ".pdf";
 
-<script>
-    function printPdf() {
-        // newWindow object can only be created by window.open()
-        // in an event listener.
-        // If we call it elsewhere, null will be returned
-        const newWindow = window.open();
+header("Content-Disposition: attachment; filename=" . urlencode($file));
+header("Content-Type: application/download");
+header("Content-Description: File Transfer");
+header("Content-Length: " . filesize($file));
 
-        // creating a new html node
-        const html = document.createElement("html");
+flush(); // This doesn't really matter.
 
-        // We can load the CSS by cloning the document head
-        // NOTE: since we are going to move node to a foreign
-        // window object, we need to clone the DOM nodes.
-        // If we don't clone, the node in this original window
-        // will disappear, because we have moved it to a new location.
-        // cloneNode(true) will perform a deep clone
-        const head = document.head.cloneNode(true);
+$fp = fopen($file, "r");
+while (!feof($fp)) {
+    echo fread($fp, 65536);
+    flush(); // This is essential for large downloads
+}
 
-        // creating a new body element for our newWindow
-        const body = document.createElement("body");
-
-        // grab the elements that you want to convert to PDF
-        const section = document
-            .getElementById("section")
-            .cloneNode(true);
-
-        // you can append as many child as you like
-        // this is where we add our elements to the new window.
-        body.appendChild(section);
-
-        html.appendChild(head);
-        html.appendChild(body);
-
-        // write content to the new window's document.
-        newWindow.document.write(html.innerHTML);
-
-        // close document to stop writing
-        // otherwise new window may hang
-        newWindow.document.close();
-
-        // print content in new window as PDF
-        newWindow.print();
-
-        // close the new window after printing
-        newWindow.close();
-    }
-</script>
-</body>
-</html>
+fclose($fp);
