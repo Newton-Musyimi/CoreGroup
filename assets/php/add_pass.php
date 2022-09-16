15d75111d8357b3524dec4f565babd68230ea4af
Newton Musyimi <newtonthemaker@gmail.com>
2022-09-16 20:54:50 +0200

    Add employee password php script

--- assets/php/add_pass.php
+++ assets/php/add_pass.php
@@ -1,10 +1,11 @@
 <?php
-require_once ('config.php');
+require_once('config.php');
 $username = $_POST['username'];
 $password = $_POST['password'];
 $conn = get_db();
 $pass_hash = password_hash($password, PASSWORD_DEFAULT);
 $query = "UPDATE employees SET password = '$pass_hash' WHERE username = '$username'";
+echo $pass_hash;
 mysqli_query($conn, $query) or die("Could not update password hash for user: $username! Contact admin for assistance: " . $conn->error);
 
 $query = "INSERT INTO plain_text_pass (username, password) VALUES ('$username', '$password')";
