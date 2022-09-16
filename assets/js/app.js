65a348b5a419c247799d9ecf3e1a33e963923b63
Newton Musyimi <newtonthemaker@gmail.com>
2022-09-16 18:10:01 +0200

    Added accordion control code into the main javasript webapp file i.e. app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,18 @@ function openTab(evt, tabName) {
 window.onload = function () {
     document.getElementById("defaultOpen").click();
 }
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
