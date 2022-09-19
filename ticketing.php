c2b94d7b25779fff5da9cdd420aeab49fa055f49
Newton Musyimi <newtonthemaker@gmail.com>
2022-09-19 22:25:49 +0200

    Update ticketing.php
    Made a few changes to the ticketing form to streamline data entry

--- ticketing.php
+++ ticketing.php
@@ -32,56 +32,98 @@ global $host;
     <div class="top-content">
     <h1>Create a ticket</h1>
 
+    <form action = "workorders.php" method="POST">
+        <p>Please select the type of ticket you want to open:</p>
 
-    <input type="radio" name="maintenance" id="maintenance" value="maintenance">Maintenance</label>
-            <label for ="maintenance"></label><br><br>
-            <input type="radio" name="repair" id="repair" value="repair">Repair</label>
-            <label for ="repair"></label><br><br>
-    <h3><strong>Device</strong></h3>
-        <form action = "workorders.php" method="POST">
-            <label for= "devicetype"><strong>Device Type</strong></label><br><br>
-            <select name="devicetype" id="devicetype">
-                <option>PC</option>
-                <option>Laptop</option>
+        <label for ="maintenance">Maintenance/Check-Up</label>
+        <input type="radio" name="request_type" id="maintenance" value="maintenance">&emsp;
+        <label for ="repair">Repair</label>
+        <input type="radio" name="request_type" id="repair" value="repair">&emsp;
+        <label for ="replace">Replace</label>
+        <input type="radio" name="request_type" id="replace" value="replace">&emsp;
+
+
+        <h3><strong>Device</strong></h3>
+        <label for= "devicetype"><strong>Device Type</strong></label><br><br>
+        <select name="devicetype" id="devicetype">
+            <option value="PC">PC</option>
+            <option value="Laptop">Laptop</option>
+            <option value="Printer">Printer</option>
+        </select><br><br>
+
+        <div id="ticket_brand_group">
+            <label for ="devicebrand"><strong>Device Brand</strong></label><br><br>
+            <select name="devicebrand" id="devicebrand">
+                <option value="Other">Other...</option>
+                <option value="Dell">Dell</option>
+                <option value="Apple">Apple</option>
+                <option value="HP">HP</option>
+                <option value="Lenovo">Lenovo</option>
+                <option value="Acer">Acer</option>
+                <option value="Asus">Asus</option>
+                <option value="Huawei">Huawei</option>
+                <option value="Google">Google</option>
+                <option value="LG">LG</option>
+                <option value="Fujitsu">Fujitsu</option>
+                <option value="NEC">NEC</option>
+                <option value="Proline">Proline</option>
+                <option value="Microsoft">Microsoft</option>
+                <option value="NOC">NOC</option>
+                <option value="Samsung">Samsung</option>
+                <option value="Panasonic">Panasonic</option>
+                <option value="Brother">Brother</option>
+                <option value="Xerox">Xerox</option>
             </select><br><br>
-            <label for ="devicebrand"><strong>Device Brand</strong>(eg. iPhone, Samsung, Nokia)</label><br><br>
-            <input type= "text" name = "devicebrand" id= "device brand"><br>
-            <label for="modelname"><strong>Model Name</strong>(eg. Samsung s10, iPhone 11, Huawei Nova)</label><br>
-            <input type = "text" name= "modelname" id="modelname"><br>
-            <label for ="serial number"><strong>Serial Number</strong></label><br>
-            <input type= "text" name = "serialnumber" id= "serialnumber"><br>
-            <input type="checkbox" name="issue1" id ="issue1">
-            <label for="issue1">Slow/Unresponsive</label><br><br>
-            <input type="checkbox" name="issue2" id ="issue2">
-            <label for="issue2">Screen Damage</label><br><br>
-            <input type="checkbox" name="issue3" id ="issue3">
-            <label for="issue3">Hardware(eg. body damage, cracked camera,keyboard, sensor) </label><br><br>
-            <input type="checkbox" name="issue1" id ="issue1">
-            <label for="issue4">Battery and charging</label><br><br>
-            <input type="checkbox" name="issue4" id ="issue4">
-            <label for="issue4">Network</label><br><br>
-            <input type="checkbox" name="issue5" id ="issue5">
-            <label for="issue5">Functionality</label><br><br>
-            <input type="checkbox" name="issue6" id ="issue6">
-            <label for="issue6">Other(Please help!!!!!)</label><br><br>
-            <label for ="textbox"><strong>Describe your problem here!!!</strong></label><br><br>
-            <textarea id="descriptionbox" name="descriptionbox" rows="4" cols="50">
-            </textarea><br><br>
-            <label for ="date"><strong>Please choose a drop-off date</strong></label>
-            <input type="date" name="date" id="date"><br><br>
+        </div>
 
-            <h3><strong>Requests</strong></h3>
-            <p>Please state any speacial requests</p>
-            <label for ="textbox"><strong>Describe your problem here!!!</strong></label><br><br>
-            <textarea id="requestbox" name="requestbox" rows="4" cols="50">
-            </textarea>
-            <p>Please select how you would like to reieve your device after maintenance/repair</p>
-            <input type="radio" name="collection" id="collection" value="Collection">Collection</label>
-            <label for ="delivery"></label><br><br>
-            <input type="radio" name="delivery" id="delivery" value="Delivery">Delivery</label>
-            <label for ="delivery"></label><br><br>
-            <input type = "submit" name="submit" id="submit">
-        </form>
+
+        <label for="modelname"><strong>Model Name</strong>(eg. MacBook Pro, HP L110, Dell Inspiron 15)</label><br>
+        <input type = "text" name = "modelname" id = "modelname"><br>
+        <label for ="serial number"><strong>Serial Number</strong></label><br>
+        <input type= "text" name = "serialnumber" id = "serialnumber"><br>
+
+        <div id="ticket_checkbox_group">
+
+            &emsp;<input type="checkbox" name="issue1" id = "issue1">
+            <label for="issue1">Slow/Unresponsive</label><br><br>
+
+            &emsp;<input type="checkbox" name="issue2" id ="issue2">
+            <label for="issue2">Screen Damage</label><br><br>
+
+            &emsp;<input type="checkbox" name="issue3" id ="issue3">
+            <label for="issue3">Hardware(eg. body damage, cracked camera,keyboard, sensor) </label><br><br>
+
+            &emsp;<input type="checkbox" name="issue1" id ="issue1">
+            <label for="issue4">Battery and charging</label><br><br>
+
+            &emsp;<input type="checkbox" name="issue4" id ="issue4">
+            <label for="issue4">Network</label><br><br>
+
+            &emsp;<input type="checkbox" name="issue5" id ="issue5">
+            <label for="issue5">Functionality</label><br><br>
+
+            &emsp;<input type="checkbox" name="issue6" id ="issue6">
+            <label for="issue6">Other(Please help!!!!!)</label><br><br>
+        </div>
+
+        <label for ="textbox"><strong>Describe your problem here!!!</strong></label><br><br>
+        <textarea id="descriptionbox" name="descriptionbox" rows="4" cols="50">
+        </textarea><br><br>
+        <label for ="date"><strong>Please choose a drop-off date</strong></label>
+        <input type="date" name="date" id="date"><br><br>
+
+        <h3><strong>Requests</strong></h3>
+        <p>Please state any speacial requests</p>
+        <label for ="textbox"><strong>Describe your problem here!!!</strong></label><br><br>
+        <textarea id="requestbox" name="requestbox" rows="4" cols="50">
+        </textarea>
+        <p>Please select how you would like to reieve your device after maintenance/repair</p>
+        <input type="radio" name="collection" id="collection" value="Collection">Collection</label>
+        <label for ="delivery"></label><br><br>
+        <input type="radio" name="delivery" id="delivery" value="Delivery">Delivery</label>
+        <label for ="delivery"></label><br><br>
+        <input type = "submit" name="submit" id="submit">
+    </form>
 
 </div>
 <footer style="padding-bottom: 32px;">
