5ac5e79d1ccf136c9061e6d43799d42c9230725d
Newton Musyimi <newtonthemaker@gmail.com>
2022-09-19 22:25:01 +0200

    Update dashboard.php
    Started working on the dashboard page

--- dashboard.php
+++ dashboard.php
@@ -25,21 +25,21 @@ require_once('security/header.php');
         function drawStuff() {
 
             var dashboard = new google.visualization.Dashboard(
-                document.getElementById('programmatic_dashboard_div'));
+                document.getElementById('ticket_by'));
 
             // We omit "var" so that programmaticSlider is visible to changeRange.
             var programmaticSlider = new google.visualization.ControlWrapper({
                 'controlType': 'NumberRangeFilter',
-                'containerId': 'programmatic_control_div',
+                'containerId': 'ticket_by_filter',
                 'options': {
-                    'filterColumnLabel': 'Donuts eaten',
+                    'filterColumnLabel': 'Number',
                     'ui': {'labelStacking': 'vertical'}
                 }
             });
 
             var programmaticChart  = new google.visualization.ChartWrapper({
                 'chartType': 'PieChart',
-                'containerId': 'programmatic_chart_div',
+                'containerId': 'chart_div',
                 'options': {
                     'width': 300,
                     'height': 300,
@@ -50,21 +50,17 @@ require_once('security/header.php');
             });
 
             var data = google.visualization.arrayToDataTable([
-                ['Name', 'Donuts eaten'],
-                ['Michael' , 5],
-                ['Elisa', 7],
-                ['Robert', 3],
-                ['John', 2],
-                ['Jessica', 6],
-                ['Aaron', 1],
-                ['Margareth', 8]
+                ['Request Type', 'Number'],
+                ['Maintenance' , 50],
+                ['Repair', 20],
+                ['Replace', 5]
             ]);
 
             dashboard.bind(programmaticSlider, programmaticChart);
             dashboard.draw(data);
 
-            changeRange = function() {
-                programmaticSlider.setState({'lowValue': 2, 'highValue': 5});
+            resetRange = function() {
+                programmaticSlider.setState({'lowValue': 0, 'highValue': 50});
                 programmaticSlider.draw();
             };
 
@@ -73,7 +69,6 @@ require_once('security/header.php');
                 programmaticChart.draw();
             };
         }
-
     </script>
 </head>
 
@@ -82,29 +77,22 @@ require_once('security/header.php');
         <?php
         getHeader();
         ?>
+        <script>
+            let current = document.getElementById("dashboard_button");
+            current.style.backgroundColor="#048337";
+            current.focus();
+        </script>
     </header>
     <div class="content-body">
-        <div id="programmatic_dashboard_div" style="border: 1px solid #ccc">
-            <table class="columns">
-                <tr>
-                    <td>
-                        <div id="programmatic_control_div" style="padding-left: 2em; min-width: 250px"></div>
-                        <div>
-                            <button style="margin: 1em 1em 1em 2em" onclick="changeRange();">
-                                Select range [2, 5]
-                            </button><br />
-                            <button style="margin: 1em 1em 1em 2em" onclick="changeOptions();">
-                                Make the pie chart 3D
-                            </button>
-                        </div>
-                    </td>
-                    <td>
-                        <div id="programmatic_chart_div"></div>
-                    </td>
-                </tr>
-            </table>
-        </div>
         <div id="dashboard">
+            <div id="ticket_by"  style="border: 1px solid #ccc">
+                <div id="ticket_by_filter" "padding-left: 2em; min-width: 250px"></div>
+            <div>
+                <button style="margin: 1em 1em 1em 2em" onclick="resetRange();">Reset</button>
+                <button style="margin: 1em 1em 1em 2em" onclick="changeOptions();">Make the pie chart 3D</button>
+            </div>
+
+            <div id="chart_div"></div>
 
         </div>
     </div>
