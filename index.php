d25046d402f6473416590fc95c09e868334e9c23
sandramuyodi <110727476+sandramuyodi@users.noreply.github.com>
2022-09-19 15:22:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,37 +19,30 @@ global $host;
     <meta http-equiv="Cache-control" content="no-store">
     <link rel="icon" type="assets/images/favicon_io/favicon-16x16.png" sizes="16x16" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
     <link rel="icon" type="assets/images/favicon_io/favicon-32x32.png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/homepage.css';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
 </head>
 
 <body id="page-top">
 
     <header value="home">
-        <div class ="navbar"> <!-- correction required here - the navbar class shouldn't be declared here because it's clashing with header.php -->
-        <div class="logo"> <!-- THIS -->
-                <img src="assets/images/Picture1.png" alt="Core group"> <!-- THIS -->
-        </div>
-                <h3>Core Group</h3> <!-- THIS -->
-                <p>Because we care.....</p> <!-- THIS --> 
+
             <?php
             getHeader();
             ?>
         </div>
     </header>
     <div class="middle-page">
-
+        <div class= "bottom-page">
+            <form action="security/login.php" method="POST">
+                <input type = "submit" value ="Create a Ticket">
+            </form>
+        </div>
     </div>
     <footer style="padding-bottom: 32px;">
         <div class="container my-auto">
             <div class="text-center my-auto copyright"><span>Copyright © Core Group 2022</span></div>
         </div>
     </footer>
-    <div class= "bottom-page">
-        <form action="security/login.php" method="POST">
-            <input type = "submit" value ="Create a Ticket">
-        </form>
-    </div>
-        
     <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
     <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
 </body>
