bb64b2a21b13aa5078e78bc82208c661b30446e4
akhonabastile <110726896+akhonabastile@users.noreply.github.com>
2022-09-19 22:30:32 +0200

    19/09/2022
    Banner for homepage is finished, Navigation bar is still a mess.

--- index.php
+++ index.php
@@ -19,19 +19,31 @@ global $host;
     <meta http-equiv="Cache-control" content="no-store">
     <link rel="icon" type="assets/images/favicon_io/favicon-16x16.png" sizes="16x16" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
     <link rel="icon" type="assets/images/favicon_io/favicon-32x32.png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/homepage.css';?>">
 </head>
 
 <body id="page-top">
-
-    <header value="home">
-
-            <?php
-            getHeader();
-            ?>
-        </div>
+    <header>
+        <?php
+        getHeader();
+        ?>
+        <section class="banner" style="background-image: url('assets/images/hardware-bg.jpg') center/ / cover;">
+            <h1>Woodstreet Academy</h1>
+            <h3>PCRepairs Department</h3>
+            <p>We offer a range of PC, laptop, phablet, and phone repairs.</p>
+            <a class="btn-bgstroke">Call To Action</a>
+        </section>
+        <script>
+            let current = document.getElementById("home_button");
+            current.style.backgroundColor="#048337";
+            current.focus();
+        </script>
     </header>
     <div class="middle-page">
+        <section class="middle">
+            <h2>We do all kinds of repair</h2>
+            <img src="assets/images/service-428540_1920.jpg">
+        </section>
         <div class= "bottom-page">
             <form action="security/login.php" method="POST">
                 <input type = "submit" value ="Create a Ticket">
--- profile.php
+++ profile.php
@@ -17,7 +17,7 @@ require_once('security/header.php');
     <meta http-equiv="Cache-control" content="no-store">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php global $host; echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/profile.css';?>">
 </head>
 
 <body id="page-top">
@@ -25,8 +25,28 @@ require_once('security/header.php');
     <?php
     getHeader();
     ?>
+    <script>
+        let current = document.getElementById("profile_button");
+        current.style.backgroundColor="#048337";
+        current.focus();
+    </script>
+    <div class="banner">
+    <h2>welcome Back Akhona</h2>
+    </div>
 </header>
 <div class="content-body">
+    <form action="" method="POST">
+        <label for="fullname">Full Name:</label>
+        <input type="text" name="fullname" id="fullname" value="Akhona Bastile"><br><br>
+        <label for="email">Email:</label>
+        <input type ="email" name="email" id="name"value="akhonabastile40@gmail.com"><br><br>
+        <label for="mobile">Mobile:</label>
+        <input type ="text" name="mobile" id="mobile"value="0846665471"><br><br>
+        <label for="address">Address:</label>
+        <input type="text" name="address" id="address" value="Grahamstown, Eastern Cape"><br><br>
+        <input type="button" value="Update">
+    </form>
+
 
 </div>
 <footer style="padding-bottom: 32px;">
