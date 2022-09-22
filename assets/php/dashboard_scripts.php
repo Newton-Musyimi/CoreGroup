3667c49733d4790126c59e7489a0c58f67f50550
Musyimi <newtonthemaker@gmail.com>
2022-09-22 05:05:38 +0200

    Update dashboard_scripts.php
    Get PHP to return the correct format of data for the graphs

--- assets/php/dashboard_scripts.php
+++ assets/php/dashboard_scripts.php
@@ -1,13 +1,14 @@
 <?php
-function getTicketByType(): bool|string
-{
-    $query = "SELECT request_type, COUNT(*) AS number FROM workorders GROUP BY request_type";
-    $result = mysqli_query($GLOBALS['conn'], $query);
-    $data = array();
-    $string = "";
-    while($row = mysqli_fetch_assoc($result)){
-        $data[] = $row;
-        $string .= "['{$row['request_type']}' , {$row['number']}],";
+require_once ('config.php');
+$conn = get_db();
+    $query = "SELECT request_type, COUNT(*) AS number FROM workorders GROUP BY request_type;";
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    //$data = array();
+    $string = "[[\"Request Type\" ,\"Number\"],";
+    while($row = mysqli_fetch_array($result)){
+        $string .= "[\"{$row['request_type']}\" , {$row['number']}],";
     }
-    return json_encode($string);
-}
+    $string = rtrim($string, ",");
+    $string .= "]";
+    mysqli_close($conn);
+    echo $string;
