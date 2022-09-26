3d902a4ed975bd6b668ed0c9cdb41855a5629cfa
Newton Musyimi <newtonthemaker@gmail.com>
2022-09-26 13:22:29 +0200

    Update config.php
    Implement session start from within the config file

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,15 +1,14 @@
 <?php
-define("SERVERNAME", "localhost");
-define("USERNAME", "g19m8045");
-define("PASSWORD", "G19M8045");
-define("DATABASE", "coregroup");
+session_start();
+define('SERVERNAME', 'is3-dev.ict.ru.ac.za');
+define('USERNAME', 'G19M8045');
+define('PASSWORD', 'G19M8045');
+define('DATABASE', 'coregroup');
 $host = "http://".$_SERVER['HTTP_HOST'];
 
 //$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE) OR die("Could not connect to MySQL: " . mysqli_connect_error() . " Contact IT for assistance!");
-function get_db(){
-    $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE) OR die("Could not connect to MySQL: " . mysqli_connect_error() . " Contact IT for assistance!");
-    return $conn;
-}
+$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASE) OR die("Could not connect to MySQL: " . mysqli_connect_error() . " Contact IT for assistance!");
+$_SESSION['db'] = $conn;
 /*
 if(isset($_SESSION['id'])){
     echo "User ID: ".$_SESSION['id']." </br>First Name: ".ucfirst($_SESSION['firstname']);
