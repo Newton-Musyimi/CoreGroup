1db43696b95115e4f21b3c6bcbb033e170b9e4fc
MattStrachan99 <109728959+MattStrachan99@users.noreply.github.com>
2022-10-04 23:47:47 +0200

    CHnaged to forgot-password

--- security/forgot-password.php
+++ security/forgot-password.php
@@ -19,7 +19,7 @@ global $host;
     <meta http-equiv="Cache-control" content="no-store">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon16.png';?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $host.'/SysDev/CoreGroup/assets/images/favicon.png';?>">
-    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/forgot_password.css';?>">
+    <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/forgot_password.css';?>">/
     <link rel="stylesheet" href="<?php echo $host.'/SysDev/CoreGroup/assets/css/style.css';?>">
 </head>
 
@@ -37,91 +37,56 @@ global $host;
         </script>
     </header>
     <div class="content-body">
-<!-- php for forgot-password-->
 <?php
 $conn=get_db();
-if(isset($_POST["email"]) && (!empty($_POST["email"]))){
-$email = $_POST["email"];
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$email = filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$email) {
-   $error .="<p>Invalid email address please type a valid email address!</p>";
-   }else{
-   $sel_query = "SELECT `users`.`username`,`users`.`table` FROM `coregroup`.`users`";
-   $results = mysqli_query($conn,$sel_query);
-   $row = mysqli_num_rows($results);
-   if ($row==""){
-   $error .= "<p>No user is registered with this email address!</p>";
-   }
-  }
-   if($error!="$email"){
-   echo "<div class='error'>".$error."</div>
-   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
-   }else{
-   $expFormat = mktime(
-   date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y"));
-   $expDate = date("Y-m-d H:i:s",$expFormat);
-   $key = md5(2418*2+$email);
-   $addKey = substr(md5(uniqid(rand(),1)),3,10);
-   $key = $key . $addKey;
-// Insert Temp Table
-mysqli_query($conn, "INSERT INTO `coregroup`.`users`(`username`,`table`)VALUES(<{username: }>,<{table: }>)");
-
-$output='<p>Dear user,</p>';
-$output.='<p>Please click on the following link to reset your password.</p>';
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p><a href="https://www.allphptricks.com/forgot-password/reset-password.php?
-key='.$key.'&email='.$email.'&action=reset" target="_blank">
-https://www.allphptricks.com/forgot-password/reset-password.php
-?key='.$key.'&email='.$email.'&action=reset</a></p>';		
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p>Please be sure to copy the entire link into your browser.
-The link will expire after 1 day for security reason.</p>';
-$output.='<p>If you did not request this forgotten password email, no action 
-is needed, your password will not be reset. However, you may want to log into 
-your account and change your security password as someone may have guessed it.</p>';   	
-$output.='<p>Thanks,</p>';
-$output.='<p>AllPHPTricks Team</p>';
-$body = $output; 
-$subject = "Password Recovery - AllPHPTricks.com";
-
-$email_to = $email;
-$fromserver = "noreply@yourwebsite.com"; 
-require("PHPMailer/PHPMailerAutoload.php");
-$mail->IsSMTP();
-$mail->Host = "mail.yourwebsite.com"; // Enter your host here
-$mail->SMTPAuth = true;
-$mail->Username = "noreply@yourwebsite.com"; // Enter your email here
-$mail->Password = "password"; //Enter your password here
-$mail->Port = 25;
-$mail->IsHTML(true);
-$mail->From = "noreply@yourwebsite.com";
-$mail->FromName = "AllPHPTricks";
-$mail->Sender = $fromserver; // indicates ReturnPath header
-$mail->Subject = $subject;
-$mail->Body = $body;
-$mail->AddAddress($email_to);
-if(!$mail->Send()){
-echo "Mailer Error: " . $mail->ErrorInfo;
+if(isset($_POST["email"]) && (!empty($_POST["email"]))) {
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $error = "";
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if (!$email) {
+        $error .= "<p>Invalid email address please type a valid email address!</p>";
+    } else {
+        $sel_query = "SELECT `users`.`table` FROM `coregroup`.`users` where `users`.`username`='$username';";
+        $results = mysqli_query($conn, $sel_query) or die($conn->error);
+        if ($row = mysqli_fetch_array($results)) {
+            $table = $row['table'];
+            $sel_query = "SELECT email FROM $table WHERE username='$username'";
+            if ($row = mysqli_num_rows($results)) {
+                echo "<form method=\"post\" action=\" \"name=\"reset\"><br><br>
+                <label for=\"password\"><strong>Enter Your New Password:</strong></label><br /><br />
+                <input type=\"password\" name=\"password\" id=\"password\"><br /><br />
+                <label for=\"confirm\"><strong>reEnter Your new Password:</strong></label><br /><br />
+                <input type=\"password\" name=\"confirm\" id=\"confirm\" />
+            <br /><br />
+            <input type=\"submit\" value=\"Reset Password\"/>
+            </form>";
+            }
+        } else {
+            $error .= "<p>No user is registered with this username!</p>";
+        }
+    }
 }else{
-echo "<div class='error'>
-<p>An email has been sent to you with instructions on how to reset your password.</p>
-</div><br /><br /><br />";
-	}
-   }
-}else{
+    echo "<form method=\"post\" action=\" \"name=\"reset\"><br><br>
+    <label for=\"username\"><strong>Username:</strong></label><br /><br />
+    <input type=\"text\" name=\"username\" id=\"username\"> <br /><br />
+    <label for=\"email\"><strong>Enter Your Email Address:</strong></label><br /><br />
+    <input type=\"email\" name=\"email\" id=\"email\" />
+<br /><br />
+<input type=\"submit\" value=\"Confirm User\"/>
+</form>";
+}
+if(isset($_REQUEST['confirm'])){
+        
+
+}
+
+      
+       
+       
 ?>
 
-<form method="post" action="" name="reset"><br/><br/>
-<label><strong>Enter Your Email Address:</strong></label><br /><br />
-<input type="email" name="email" placeholder="username@email.com" />
-<br /><br />
-<input type="submit" value="Reset Password"/>
-</form>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<?php } ?>
     <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/app.js';?>"></script>
     <script src="<?php echo $host.'/SysDev/CoreGroup/assets/js/theme.js';?>"></script>
 </body>
