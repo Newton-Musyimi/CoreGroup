b14b300cf7a82ebe8e9c3a473a9ccf3a77b34a22
Musyimi <newtonthemaker@gmail.com>
2022-10-05 14:31:44 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,26 +1,26 @@
 (function() {
   "use strict"; // Start of use strict
 
-  var sidebar = document.querySelector('.sidebar');
-  var sidebarToggles = document.querySelectorAll('#sidebarToggle, #sidebarToggleTop');
+  let navbar = document.querySelector('.navbar');
+  let navbarToggles = document.querySelectorAll('#navbarToggle, #navbarToggleTop');
   
-  if (sidebar) {
+  if (navbar) {
     
-    var collapseEl = sidebar.querySelector('.collapse');
-    var collapseElementList = [].slice.call(document.querySelectorAll('.sidebar .collapse'))
-    var sidebarCollapseList = collapseElementList.map(function (collapseEl) {
+    let collapseEl = navbar.querySelector('.collapse');
+    let collapseElementList = [].slice.call(document.querySelectorAll('.navbar .collapse'))
+    let navbarCollapseList = collapseElementList.map(function (collapseEl) {
       return new bootstrap.Collapse(collapseEl, { toggle: false });
     });
 
-    for (var toggle of sidebarToggles) {
+    for (let toggle of navbarToggles) {
 
       // Toggle the side navigation
       toggle.addEventListener('click', function(e) {
-        document.body.classList.toggle('sidebar-toggled');
-        sidebar.classList.toggle('toggled');
+        document.body.classList.toggle('navbar-toggled');
+        navbar.classList.toggle('toggled');
 
-        if (sidebar.classList.contains('toggled')) {
-          for (var bsCollapse of sidebarCollapseList) {
+        if (navbar.classList.contains('toggled')) {
+          for (let bsCollapse of navbarCollapseList) {
             bsCollapse.hide();
           }
         };
@@ -29,10 +29,10 @@
 
     // Close any open menu accordions when window is resized below 768px
     window.addEventListener('resize', function() {
-      var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+      let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
       if (vw < 768) {
-        for (var bsCollapse of sidebarCollapseList) {
+        for (let bsCollapse of navbarCollapseList) {
           bsCollapse.hide();
         }
       };
@@ -41,14 +41,14 @@
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   
-  var fixedNaigation = document.querySelector('body.fixed-nav .sidebar');
+  let fixedNavigation = document.querySelector('body.fixed-nav .navbar');
   
-  if (fixedNaigation) {
-    fixedNaigation.on('mousewheel DOMMouseScroll wheel', function(e) {
-      var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  if (fixedNavigation) {
+    fixedNavigation.on('mousewheel DOMMouseScroll wheel', function(e) {
+      let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
       if (vw > 768) {
-        var e0 = e.originalEvent,
+        let e0 = e.originalEvent,
           delta = e0.wheelDelta || -e0.detail;
         this.scrollTop += (delta < 0 ? 1 : -1) * 30;
         e.preventDefault();
@@ -56,13 +56,13 @@
     });
   }
 
-  var scrollToTop = document.querySelector('.scroll-to-top');
+  let scrollToTop = document.querySelector('.scroll-to-top');
   
   if (scrollToTop) {
     
     // Scroll to top button appear
     window.addEventListener('scroll', function() {
-      var scrollDistance = window.pageYOffset;
+      let scrollDistance = window.pageYOffset;
 
       //check if user is scrolling up
       if (scrollDistance > 100) {
